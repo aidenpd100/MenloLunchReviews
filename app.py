7a0d6b6d9f734be7085f614bc9e386d655aae434
Aiden Deffner <aidenpd100@gmail.com>
2023-05-03 14:58:05 -0700

    Signup/Login/Logout functionality

--- app.py
+++ app.py
@@ -5,13 +5,69 @@ from models import *
 app = Flask(__name__)
 
 # TODO: Change the secret key
-app.secret_key = "Change Me"
+app.secret_key = "vwnWWDM+igMGbmvsOw=="
 
-# TODO: Fill in methods and routes
+# TODO: Fill in methods and routes  
 
-@app.before_first_request
-def setup():
-    init_db()
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        users = db_session.query(User).all()
+        for user in users:
+            if user.username == username and user.password == password:
+                session["username"] = username
+                return redirect(url_for("review"))
+            
+        flash("Incorrect Username/Password", "error")
+        return render_template("login.html")
+        
+
+
+@app.route("/review", methods=["GET", "POST"])
+def review():
+    if request.method == "GET":
+        if "username" in session:
+            return render_template("review.html")
+        else:
+            return redirect(url_for("login"))
+    
+@app.route("/request", methods=["GET", "POST"])
+def requests():
+    if request.method == "GET":
+        return render_template("request.html")
+    
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        re_password = request.form["re-password"]
+        admin_code = request.form["admin_code"]
+
+        if password != re_password:
+            flash("Passwords Don't Match", "error")
+            return render_template("signup.html")
+        elif admin_code == 'code':
+            db_session.add(User(username=username, password=password, is_admin=True))
+        else:
+            db_session.add(User(username=username, password=password, is_admin=False))
+        
+        db_session.commit()
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    if "username" in session:
+        session.pop("username")
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -4,9 +4,43 @@ that will be used to create objects
 and connect to data tables.
 """
 
-from sqlalchemy import ForeignKey, Column, INTEGER, TEXT
+from sqlalchemy import ForeignKey, Column, INTEGER, TEXT, BOOLEAN
 from sqlalchemy.orm import relationship
 from database import Base
 
 # TODO: Complete your models
-    
+class User(Base):
+    __tablename__ = "users"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    username = Column("username", TEXT, nullable=False)
+    password = Column("password", TEXT, nullable=False)
+    is_admin = Column("is_admin", BOOLEAN, nullable=False)
+
+class Lunch(Base):
+    __tablename__ = "lunches"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    title = Column("title", TEXT, nullable=False)
+    avg_rating = Column("avg_rating", INTEGER, nullable=True)
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    user_id = Column("user_id", ForeignKey("users.id"))
+    lunch_id = Column("lunch_id", ForeignKey("lunches.id"))
+    rating = Column("rating", INTEGER, nullable=False)
+    feedback = Column("feedback", TEXT, nullable=True)
+
+class Request(Base):
+    __tablename__ = "requests"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    user_id = Column("user_id", ForeignKey("users.id"))
+    title = Column("title", TEXT, nullable=False)
+    description = Column("description", TEXT, nullable=True)
